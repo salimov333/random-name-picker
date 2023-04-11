0432330e0018f83ea2464ad2828a6f70652ff49a
salimov333 <salimov333@gmail.com>
2023-04-11 18:27:40 +0200

    slider done

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ function pickWinner() {
     const validNames = inputNames.filter(name => name.trim() !== '');
     const winnerElement = document.getElementById("winner");
     const winnerList = document.getElementById("winner_list");
+    const buttonPick = document.querySelector(".btn-pick");
 
     if (validNames.length === 0 && names.length === 0) {
         alert("Please enter some names!");
@@ -52,34 +53,27 @@ function pickWinner() {
     const allNames = Array.from(new Set(validNames.concat(names)));
 
     if (allNames.length === 0) {
-        alert("All names have been picked, enter new names!");
+        alert("All names have been picked!");
         return;
     }
 
     const randomIndex = Math.floor(Math.random() * allNames.length);
     const winner = allNames[randomIndex];
     names = allNames.filter(name => name !== winner);
-    document.getElementById("names").value = names.join(",");
-
-
 
     if (names.length === 0) {
-        // remove winner-picked class from #winner element
-        winnerElement.classList.remove("winner-picked");
-        winnerElement.innerHTML = `All names have been picked, enter new names!`;
+        winnerElement.style.fontSize = "1rem";
+        winnerElement.innerHTML = `All names have been picked!`;
     } else {
-
-        // add winner-picked class to #winner element
-        winnerElement.classList.add("winner-picked");
-        winnerElement.innerHTML = `The winner is ${winner}!`;
-        // trigger bounceIn animation by adding/removing bounceIn class
         setTimeout(() => {
-            winnerElement.classList.add("bounceIn");
-            setTimeout(() => {
-                winnerElement.classList.remove("bounceIn");
-            }, 1000);
-        }, 10);
-        winnerList.innerHTML = `${winner}`;
+            // add chosen-name class to #winner element
+            winnerElement.classList.add("chosen-name")
+            winnerElement.textContent = winner
+        }, 500);
+        setTimeout(() => {
+            winnerElement.textContent = ""
+            winnerElement.classList.remove("chosen-name")
+            winnerElement.appendChild(winnerList)
+        }, 6000)
     }
 };
-
