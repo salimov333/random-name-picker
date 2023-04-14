06b68834f471c2842573dfcbe640366fcde1e22d
salimov333 <salimov333@gmail.com>
2023-04-15 00:16:16 +0200

    added delete names button

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const namesContainer = document.getElementById("names_container");
 const buttonPick = document.querySelector(".btn-pick");
 const buttonNewRound = document.querySelector(".btn-newRound");
 const buttonAdd = document.querySelector(".btn-add");
+const buttonDel = document.querySelector(".btn-del");
 const spanNumber = document.querySelector(".number");
 const spanNameWord = document.querySelector(".name_word");
 
@@ -21,16 +22,21 @@ const spanNameWord = document.querySelector(".name_word");
     //Enable new round button.
     buttonNewRound.disabled = false;
 
-    //If no names are added, disable the Add names button.
+    //If no names are entered, disable the add names button and delete names button.
     buttonAdd.disabled = true;
+    buttonDel.disabled = true;
 
     inputNamesEl.addEventListener("input", () => {
         if (inputNamesEl.value.length === 0) {
             buttonAdd.disabled = true;
+            buttonDel.disabled = true;
         } else {
             buttonAdd.disabled = false;
+            buttonDel.disabled = false;
         }
     });
+
+    spanNameWord.innerHTML = namesArr.length < 2 ? "name" : "names";
 
     //Create and fill the names list based on the names array.
     createNamesList(namesArr);
@@ -40,7 +46,8 @@ const spanNameWord = document.querySelector(".name_word");
 //Basic App Function to pick and show a name. It is triggered when the button `Pick a name` i clicked.
 function pickName() {
 
-    addNames();
+    namesContainer.classList.remove("empty");
+    inputNamesEl.value = "";
 
     //Generate a random integer between 0 and the index of last name in the names array
     const randomIndex = randomInteger(0, namesArr.length - 1);
@@ -70,6 +77,7 @@ function pickName() {
         namesList.classList.add("accelerate");
         buttonPick.disabled = true;
         buttonAdd.disabled = true;
+        buttonDel.disabled = true;
         setTimeout(() => {
             namesContainer.classList.add("picked_name");
             namesContainer.innerHTML = pickedName;
@@ -78,6 +86,7 @@ function pickName() {
         setTimeout(() => {
             buttonPick.disabled = false;
             buttonAdd.disabled = false;
+            buttonDel.disabled = false;
             namesList.classList.remove("accelerate");
             namesContainer.classList.remove("picked_name");
             createNamesList(namesArr);
@@ -120,11 +129,14 @@ function newRound() {
     // Save the Default names array into localStorage.
     localStorage.setItem("namesArrStorage", JSON.stringify(DefaultNamesArr));
     namesArr = JSON.parse(localStorage.getItem("namesArrStorage"));
+
+    spanNameWord.innerHTML = namesArr.length < 2 ? "name" : "names";
+    
     createNamesList(namesArr);
     console.log("namesArr", namesArr);
 };
 
-//App Function to add new names based on input names. It is triggered when the button `Add names` i clicked.
+//App Function to add new names based on input names. It is triggered when the button `Add names` is clicked.
 function addNames() {
     //Enable new round button.
     buttonNewRound.disabled = false;
@@ -133,9 +145,11 @@ function addNames() {
 
     //Get input names and remove the spaces .
     const inputNames = inputNamesEl.value.split(",");
+    const validInputName = inputNames.map(name => name.trim());
+    console.log("validInputName", validInputName);
     inputNamesEl.value = "";
-    const validNames = inputNames.filter(name => name.trim() !== '');
-    console.log("Added names:", validNames);
+    const validNames = validInputName.filter(name => name.trim() !== '');
+    console.log("To add names:", validNames);
 
     //Alert no input names and empty names array!
     if (validNames.length === 0 && namesArr.length === 0) {
@@ -143,15 +157,65 @@ function addNames() {
         alert("Please enter some names or start a new round!");
         return;
     };
-    
-    //remove repeated names and update names array.
+    //Alert no input names
+    if (validNames.length === 0) {
+        alert("Please enter some names to add");
+        return;
+    };
+
+    //Remove repeated names and update names array.
     namesArr = Array.from(new Set(validNames.concat(namesArr)));
+    console.log("namesArr after add", namesArr);
 
     // Save the names array into localStorage.
     localStorage.setItem("namesArrStorage", JSON.stringify(namesArr));
 
+    spanNameWord.innerHTML = namesArr.length < 2 ? "name" : "names";
+
     createNamesList(namesArr);
-}
+};
+
+//App Function to delete names based on input names. It is triggered when the button `Delete names` is clicked.
+function delNames() {
+    //Enable new round button.
+    buttonNewRound.disabled = false;
+
+    namesContainer.classList.remove("empty");
+
+    //Get input names and remove the spaces .
+    const inputNames = inputNamesEl.value.split(",");
+    const validInputName = inputNames.map(name => name.trim());
+    console.log("validInputName", validInputName);
+    inputNamesEl.value = "";
+    const validNames = validInputName.filter(name => name.trim() !== '');
+    console.log("To delete names:", validNames);
+
+    //Alert no input names and empty names array!
+    if (validNames.length === 0 && namesArr.length === 0) {
+        namesContainer.classList.add("empty");
+        alert("Please enter some names or start a new round!");
+        return;
+    };
+
+    //Alert no input names
+    if (validNames.length === 0) {
+        alert("Please enter some names to delete");
+        return;
+    };
+
+    //Remove the common items from the validNames an namesArr.
+    const uniqueNames = namesArr.filter(name => !validNames.includes(name));
+    namesArr = uniqueNames;
+    console.log("uniqueNames", uniqueNames);
+    console.log("namesArr after delete", namesArr);
+
+    // Save the names array into localStorage.
+    localStorage.setItem("namesArrStorage", JSON.stringify(namesArr));
+
+    spanNameWord.innerHTML = namesArr.length < 2 ? "name" : "names";
+
+    createNamesList(namesArr);
+};
 
 //Auxiliary Functions.
 
